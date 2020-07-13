99e3d971204d95fae808e32be87426446cbca57c
Alexandre Manoel da Silva <alemanoelsilva@gmail.com>
2020-07-13 19:52:58 -0300

    new simple endpoint

--- simple_node_api/index.js
+++ simple_node_api/index.js
@@ -16,6 +16,15 @@ const init = async () => {
 
     });
 
+    app.get('/test/oi', (request, response) => {
+        console.log('\n\nEITA', request.config)
+
+        response.status(200).json({
+            message: 'oi mano'
+        }).end();
+
+    });
+
     await app.listen(3030);
 
     console.log('estamos on the line')
